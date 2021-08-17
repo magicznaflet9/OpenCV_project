97174eff4cfaf8663da87ce0fc72b57b6c9d0116
Zofia <zos.boc@gmail.com>
2021-08-17 11:59:47 +0200

    pierwsze testy

--- test_pierwszej_foki.py
+++ test_pierwszej_foki.py
@@ -3,7 +3,9 @@ print("I got here")
 img = cv2.imread("/Users/zofiabochenek/test/FOKA.png")
 cv2.imshow("puszysta foczka",img)
 cv2.waitKey(0)
+img = cv2.imread("/Users/zofiabochenek/test/FOKA.png",cv2.IMREAD_GRAYSCALE)
+cv2.imshow("puszysta foczka w skali szarości",img)
+cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
 print("Pamiętaj zabrać skarpety")
-
-
-
---
+++ testy.py
@@ -0,0 +1,16 @@
+# Python program to explain cv2.imread() method
+ 
+# importing cv2
+import cv2
+ 
+# path
+path = "/Users/zofiabochenek/test/FOKA.png"
+ 
+# Using cv2.imread() method
+# Using 0 to read image in grayscale mode
+img = cv2.imread(path, 0)
+ 
+# Displaying the image
+cv2.imshow('image', img)
+cv2.waitKey(0)
+
